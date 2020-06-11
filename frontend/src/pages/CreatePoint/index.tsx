843aa0d455efbd3dde5d5e3ef9cbc30f590fe194
Lucas Fernandes <lucas-fernandes87@hotmail.com>
2020-06-11 14:03:29 -0300

    Criando a pagina Home e definindo a estrutura basica do CreatePoint

---
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+const    CreatePoint =()=> {
+    return (
+        <div>
+            <div>Criação do ponto de coleta</div>
+        </div>
+    )
+}
+export default CreatePoint;
